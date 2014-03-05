490a9da40e00dc281236141034e0cce379124ae5
xiaowenlh <270723114@qq.com>
2014-03-05 17:44:27 +0800

    不记得了

--- app/controllers/GetmsgController.php
+++ app/controllers/GetmsgController.php
@@ -7,14 +7,16 @@ class GetmsgController extends BaseController {
      * @var Post
      */
     protected $orders;
+    protected $ordersmsg;
     /**
      * Inject the models.
      * @param Post $post
      */
-    public function __construct(Orders $orders)
+    public function __construct(Orders $orders,Ordersmsgs $ordersmsgs)
     {
         parent::__construct();
         $this->orders = $orders;
+        $this->ordersmsgs = $ordersmsgs;
     }
 
     /**
@@ -24,9 +26,29 @@ class GetmsgController extends BaseController {
      */
     public function getIndex()
     {
-$data = json_decode(file_get_contents("php://input"));
-        //$data = Input::json();
-        return $data;
-        //return View::make('site/blog/index', compact('data'));
+        $data = json_decode(file_get_contents("php://input"));
+        $this->orders->name='a';
+        $this->orders->phone='123456';
+        $this->orders->remark='good';
+        $this->orders->pay='none';
+        $this->orders->save();
+        $id=$this->orders->id;
+        $i=0;
+        $price=0;
+        $str=array();
+        foreach($data as $d)
+        {
+            $this->ordersmsgs->name=$d->name;
+            $this->ordersmsgs->count=$d->count;
+            $this->ordersmsgs->price=$d->price;
+            //$this->ordersmsgs->save();
+            $price=$price+(int)($d->price);
+            //echo $this->ordersmsgs->name."\n";
+            $str[]=['name'=>$this->ordersmsgs->name,'count'=>$this->ordersmsgs->count,'price'=>$this->ordersmsgs->price,'ofid'=>$id];
+            $i++;
+        }
+        $this->ordersmsgs->Insert($str);
+        $affectedRows = Orders::where('id', '=', $id)->update(array('price' => $price));
+        echo $price;
     }
 }
