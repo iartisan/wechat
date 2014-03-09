8ed5066938dcba24897c1d621ae6913e84815c07
LokiSeven <seven5272@gmail.com>
2014-03-09 22:15:46 +0000

    修改跳转

--- public/angular/app/js/controllers.js
+++ public/angular/app/js/controllers.js
@@ -155,7 +155,7 @@ angular.module('myApp.controllers', []).
 
       $scope.pushOrder = function(){
           $http.post('getmsg',$scope.order).success(function(data,status,headers,config){
-              //$location.path('/orderSuccess');
+              $location.path('/orderSuccess');
           }).error()
       }
   }])
